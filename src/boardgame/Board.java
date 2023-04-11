78974b5b73c82b2820e0c13e204cbaf070b410f9
Alexsandro Alves <alexsandroalves1297@gmail.com>
2023-04-11 18:37:22 -0300

    BoardException and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -8,6 +8,9 @@ public class Board {
 	private Piece [][] pieces;
 	
 	public Board(int rows, int columns) {
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Error crating board: there must be at least 1 row and 1 column");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece [rows][columns];
@@ -17,29 +20,46 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
 	}
-
-	public void setColumns(int columns) {
-		this.columns = columns;
+	
+	public Piece piece(int row, int column) {
+		if (!positionExists(row, column)) {
+			throw new BoardException("Position not on the bboard");
+		}
+		return pieces[row][column];
 	}
 	
-	public Piece piece (int row, int column) {
-		return pieces [row][column];
-	}
-	
-	public Piece piece (Position position) {
-		return pieces [position.getRow()][position.getColumn()];
+	public Piece piece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the bboard");
+		}
+		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece(Piece piece, Position position) {
+		if (thereIsAPIece(position)) {
+			throw new BoardException("There is already a piece on pisition " + position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	
+	private boolean positionExists(int row, int column) {
+		return row >=0 && row < rows && column >= 0 && column < columns;
+		
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPIece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the bboard");
+		}
+		return piece(position) != null;
 	}
 }
 	
